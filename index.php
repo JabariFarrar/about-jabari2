e3fa5a2c212f648fb388b9fa3211b2511953eb46
Jabari Farrar <jofarrar@gmail.com>
2017-04-10 19:08:21 -0600

    added a paragraph about myself

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title> About Jabari
+		</title>
+		<body>
+		<p>My name is Jabari Farrar and I just moved to Albuquerque, NM late last year</p>
+		<p> I am a surace scientist who enjoys materials characterization for a living</p>
+		<p> I am married and have two Jack Rusell Terriers</p>
+		<p> Belows is a picture of one them</p>
+		<img src="">
+	</body>
+	</head>
+</html>
